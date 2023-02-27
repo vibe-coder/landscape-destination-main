8db23a6c2a385c392d5f9e2dbed91659d6078d1c
vibe_coder <67506007+vibe-coder@users.noreply.github.com>
2023-02-27 22:32:38 +0100

    Hero Section

--- landscape-destination/src/App.js
+++ landscape-destination/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Header from './Header';
-import Background from './Background';
+import Body from './Body';
 
 function App() {
   return (
     <div className="App">
-      <Header className="header"/>
+      <Header/>
+      <Body />
     </div>
   );
 }
---
+++ landscape-destination/src/Body.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css"
+
+
+function Body(){
+  return(
+    <div className="body-wrapper">
+      <div className="line">
+        <div className="circle">.</div>
+        <div className="circle">.</div>
+        <div className="circle">.</div>
+      </div>
+
+      <div className="hero-wrapper">
+        <h1 className="hero-text">INDONESIA</h1>
+        <p className="hero-text">Anim sit qui occaecat excepteur Lorem nostrud nostrud veniam. Proident enim duis duis pariatur ut dolore laboris. Fugiat proident voluptate occaecat aliqua duis ad deserunt ipsum enim. Incididunt adipisicing ea aute tempor sint adipisicing ipsum magna mollit. Consectetur labore tempor id eu ex laboris dolor excepteur. Incididunt sit veniam cillum nostrud veniam dolore eu minim ex.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Body;
